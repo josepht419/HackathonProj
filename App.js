02fafc2c7d2ae9416cfe147436a4b281f20ec131
feliasdager <fdage003@fiu.edu>
2021-08-06 13:11:46 -0400

    added a map and simple login page

--- App.js
+++ App.js
@@ -1,14 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View} from 'react-native'; 
+//import { NavigationContainer } from "@react-navigation/native";  
+import { NativeRouter, Switch, Route } from 'react-router-native';
+import LogIn from "./LogIn"
+import Profile from "./Profile" 
+import GymMap from "./GymMap"
+
+
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Text>Testing!</Text>
-      <StatusBar style="auto" />
-    </View>
+  return ( 
+    <NativeRouter>
+      <Switch>
+        <Route exact path = "/" component={LogIn}></Route> 
+        <Route exact path = "/Profile" component={Profile}></Route> 
+        <Route exact path = "/GymMap" component = {GymMap}></Route>
+      </Switch>
+    </NativeRouter>
   );
 }
 
